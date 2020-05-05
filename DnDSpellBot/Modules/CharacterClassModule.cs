655761edd5501deea3033604b003d6714b9e29b4
morgan-mcnabb <morgan.mcnabb@protonmail.com>
2020-05-04 23:21:51 -0400

    Added a help command so users can see what commands are available, how to use them and what they return.

--- DnDSpellBot/Modules/CharacterClassModule.cs
+++ DnDSpellBot/Modules/CharacterClassModule.cs
@@ -30,11 +30,10 @@ namespace DnDSpellBot.Modules
             int offset = printData.Length / loopNum;
 
             //max reply length is 2000 characters, all class data have 7000+ characters
-            for(int i = 0; i < loopNum; i++)
+            for (int i = 0; i < loopNum; i++)
             {
                 await ReplyAsync(printData.Substring(i * offset, offset));
             }
-
         }
 
         [Command("allclasses")]
--- DnDSpellBot/Modules/InfoModule.cs
+++ DnDSpellBot/Modules/InfoModule.cs
@@ -13,19 +13,62 @@ namespace DnDSpellBot.Modules
 {
     public class InfoModule : ModuleBase
     {
-        [Command("hello")]
+        [Command("help")]
         [Summary("Echoes a message")]
         public async Task HelloCommand()
         {
-            var sb = new StringBuilder();
+            StringBuilder helpText = new StringBuilder();
+            helpText.Append("Thank you for using this Dungeons and Dragons bot!");
+            helpText.Append("\n");
+            helpText.Append("\n");
 
-            var user = Context.User;
+            helpText.Append("Each command starts with the ! character.");
+            helpText.Append("\n");
+            helpText.Append("\n");
 
-            sb.AppendLine($"You are -> [{user.Username}]");
+            helpText.Append("List of commands and what they do: ");
+            helpText.Append("\n");
+            helpText.Append("\n");
 
-            sb.AppendLine("Hello World");
+            helpText.Append("!classes - returns a list of all 5E classes to choose from.");
+            helpText.Append("\n");
+            helpText.Append("\n");
 
-            await ReplyAsync(sb.ToString());
+            helpText.Append("!class [class name] - !class followed by the name of the class returns all details from a class.");
+            helpText.Append("\n");
+            helpText.Append("---NOTE--- due to discords 2000 character limit, these messages will come in multiple messages. Give them time to print properly.");
+            helpText.Append("\n");
+            helpText.Append("\n");
+
+            helpText.Append("!magicitem [magic item name] - !magicitem followed by the name of the item will return details about the magic item.");
+            helpText.Append("\n");
+            helpText.Append("\n");
+
+            helpText.Append("!monster [monster name] - !monster followed by the monster's name will return relevant information about the monster.");
+            helpText.Append("\n");
+            helpText.Append("\n");
+
+            helpText.Append("!monstersbycr [Challenge Rating] - !monstersbycr followed by the Challenge Rating will return all monsters with that Challenge Rating.");
+            helpText.Append("\n");
+            helpText.Append("\n");
+
+            helpText.Append("!spell [spell name] - !spell followed by the spell's name will return all relevant information for the spell.");
+            helpText.Append("\n");
+            helpText.Append("\n");
+
+            helpText.Append("!spellsfor [class name] - !spellsfor followed by the name of the class will return all spells that class can learn, ordered by spell level.");
+            helpText.Append("\n");
+            helpText.Append("\n");
+
+            helpText.Append("!weapon [weapon name] - !weapon followed by the weapon's name will return all relevant information regarding the weapon entered. ");
+            helpText.Append("\n");
+            helpText.Append("\n");
+
+            helpText.Append("!allweapons - !allweapons will return all simple and martial weapons available in 5E.");
+
+            string printData = helpText.ToString();
+
+            await ReplyAsync(printData);
         }
     }
 }
